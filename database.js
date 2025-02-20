ffa77162e411d4309202b6a577eebdc9f1e88951
crystalrobinson149 <crystalrobinson149@gmail.com>
2025-02-20 08:32:57 -0500

    Update database.js

--- database.js
+++ database.js
@@ -7,15 +7,15 @@ const wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
  * @return {String} The name of the database to look up the user.
  */
 export async function central(id) {
-    if (typeof id !== "number") throw new Error("Invalid Input -- Not a Number");
-    if (id < 1 || id > 10) throw new Error("Invalid Input -- Out of Range");
+  if (typeof id !== "number") throw new Error("Invalid Input -- Not a Number");
+  if (id < 1 || id > 10) throw new Error("Invalid Input -- Out of Range");
 
-    await wait(100); // Faking an "external" call here by adding a delay.
-    let db = "db1";
-    if (id > 4) db = "db2";
-    if (id > 7) db = "db3";
+  await wait(100); // Faking an "external" call here by adding a delay.
+  let db = "db1";
+  if (id > 4) db = "db2";
+  if (id > 7) db = "db3";
 
-    return db;
+  return db;
 }
 
 /**
@@ -25,17 +25,17 @@ export async function central(id) {
  * @return {Object} The user's username, website, and company data.
  */
 export async function db1(id) {
-    if (typeof id !== "number") throw new Error("Invalid Input -- Not a Number");
-    if (id < 1 || id > 4) throw new Error("Invalid Input -- Out of Range");
+  if (typeof id !== "number") throw new Error("Invalid Input -- Not a Number");
+  if (id < 1 || id > 4) throw new Error("Invalid Input -- Out of Range");
 
-    const data = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
-    const json = await data.json();
+  const data = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
+  const json = await data.json();
 
-    return {
-        username: json.username,
-        website: json.website,
-        company: json.company
-    };
+  return {
+    username: json.username,
+    website: json.website,
+    company: json.company
+  };
 }
 /**
  * Accepts a user id value and returns an object containing
@@ -44,17 +44,17 @@ export async function db1(id) {
  * @return {Object} The user's username, website, and company data.
  */
 export async function db2(id) {
-    if (typeof id !== "number") throw new Error("Invalid Input -- Not a Number");
-    if (id < 5 || id > 7) throw new Error("Invalid Input -- Out of Range");
+  if (typeof id !== "number") throw new Error("Invalid Input -- Not a Number");
+  if (id < 5 || id > 7) throw new Error("Invalid Input -- Out of Range");
 
-    const data = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
-    const json = await data.json();
+  const data = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
+  const json = await data.json();
 
-    return {
-        username: json.username,
-        website: json.website,
-        company: json.company
-    };
+  return {
+    username: json.username,
+    website: json.website,
+    company: json.company
+  };
 }
 /**
  * Accepts a user id value and returns an object containing
@@ -63,17 +63,17 @@ export async function db2(id) {
  * @return {Object} The user's username, website, and company data.
  */
 export async function db3(id) {
-    if (typeof id !== "number") throw new Error("Invalid Input -- Not a Number");
-    if (id < 8 || id > 10) throw new Error("Invalid Input -- Out of Range");
+  if (typeof id !== "number") throw new Error("Invalid Input -- Not a Number");
+  if (id < 8 || id > 10) throw new Error("Invalid Input -- Out of Range");
 
-    const data = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
-    const json = await data.json();
+  const data = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
+  const json = await data.json();
 
-    return {
-        username: json.username,
-        website: json.website,
-        company: json.company
-    };
+  return {
+    username: json.username,
+    website: json.website,
+    company: json.company
+  };
 }
 
 /**
@@ -83,16 +83,16 @@ export async function db3(id) {
  * @return {Object} The user's name, email, address, and phone data.
  */
 export async function vault(id) {
-    if (typeof id !== "number") throw new Error("Invalid Input -- Not a Number");
-    if (id < 1 || id > 10) throw new Error("Invalid Input -- Out of Range");
+  if (typeof id !== "number") throw new Error("Invalid Input -- Not a Number");
+  if (id < 1 || id > 10) throw new Error("Invalid Input -- Out of Range");
 
-    const data = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
-    const json = await data.json();
+  const data = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
+  const json = await data.json();
 
-    return {
-        name: json.name,
-        email: json.email,
-        address: json.address,
-        phone: json.phone
-    };
+  return {
+    name: json.name,
+    email: json.email,
+    address: json.address,
+    phone: json.phone
+  };
 }
