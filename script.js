b6c74e13b50df1e4957b384a5806475972a3f8c2
crystalrobinson149 <crystalrobinson149@gmail.com>
2025-02-20 08:32:18 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,61 +2,71 @@
 import { central, db1, db2, db3, vault } from "./databases.js";
 
 
-console.log("================ central ====================")
 // central: database identifies which database the users are stored within
-const val = await central(1);
+const val = await central(7);
 console.log(val); // returns-> db1
 
-
-console.log("================ db1 ====================")
 // db1, db2. db3: databases contain the user's basic information, including username, website, and company.
-const val2 = await db1(1)
+const val2 = await db1(3)
 console.log(val2);
 
-console.log("================ vault ====================")
 // val: The personal data for each user is contained within the vault database since its access and usage is restricted by law.
-const val3 = await vault(1);
+const val3 = await vault(6);
 console.log(val3);
 
-console.log("============================================")
+central(10).then(db => console.log(db))
+db3(10).then(userBasicInfo => console.log(userBasicInfo))
+vault(10).then(userPersonalInfo => console.log(userPersonalInfo))
 
-// todo: solve by =>    Promise chaining via then() statements
-// todo: then =>        async/await syntax
-
-// ? GIVEN  =>  I have 3 diﬀerent databases 
-// ?            each database request takes 100ms to respond
-
-// todo: your function must complete in 200ms or less
-// todo: Make use of Promise.all to handle requests concurrently where applicable.
-
-
-//? central: is db identifies which db the users are stored within.
-//? return a string that identifies which db to access for that user's information
 
 
 function getUserData(id) {
-    const dbs = {
-        db1: db1,
-        db2: db2,
-        db3: db3
-    };
-
-    // Step 1: Get db name
-    return central(id)
-        .then(dbName => {
-            // Get basic user info
-            console.log(`HERE DB Name := ${dbName}`);
-            
-        })
+  const dbs = {
+    db1: db1,
+    db2: db2,
+    db3: db3,
+  };
 }
 
+async function getUserInfo(id) { 
+  if (typeof id !== "number" || id < 1 || id > 10) {
+      return Promise.reject(new Error("Invalid ID: ID must be a number between 1 and 10"));
+  }
 
-//! Test cases:
-//! test your code by passing it many different values for id, including:
-//!     1. Valid numbers – 1 through 10 (inclusive).
-//!     2. Invalid numbers – less than 1 or higher than 10.
-//!     3. Invalid data types – strings, Booleans, etc.
+  try {
+      const dbs = await central(id);
 
+      const [userData, addressData, contactData] = await Promise.all([
+          eval(dbName)(id), 
+          address(id),
+          contact(id)
+      ]);
 
-let user = await getUserData(1);
-console.log(user);
+      return {
+          id: id,
+          name: contactData.name,
+          username: userData.username,
+          email: contactData.email,
+          address: {
+              street: addressData.street,
+              suite: addressData.suite,
+              city: addressData.city,
+              zipcode: addressData.zipcode,
+              geo: {
+                  lat: addressData.geo.lat,
+                  lng: addressData.geo.lng
+              }
+          },
+          phone: contactData.phone,
+          website: userData.website,
+          company: {
+              name: userData.company.name,
+              catchPhrase: userData.company.catchPhrase,
+              bs: userData.company.bs
+          }
+      };
+  } catch (error) {
+      return Promise.reject(new Error(`Failed to fetch user data: ${error.message}`));
+  }
+}
+getUserInfo(9);
